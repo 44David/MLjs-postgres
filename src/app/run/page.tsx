72bc438f672c8e49f98b0f9ed7574c2bc98c8a14
David Soane <david.soane@outlook.com>
2024-07-01 14:35:10 -0600

    init commit

---
+++ src/app/run/page.tsx
@@ -0,0 +1,8 @@
+export default function Run() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className="font-black text-7xl">MLjs</h1>
+        <h3>Run LLM's in the browser and build custom models</h3>
+      </main>
+    );
+  } 
