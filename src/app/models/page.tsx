b6899e9568a77006e4c87c546af5e48f571efa75
David Soane <david.soane@outlook.com>
2024-07-03 20:26:31 -0600

    modified models router UI

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -18,10 +18,13 @@ export default function Models() {
         <>
             <h1>Currently available models:</h1>
 
-            <div className='p-4 outline w-1/4 gap-2'>
-                <h2>Llama 3</h2>
+            <div className='p-4 outline w-1/4 gap-2 outline-gray-500 rounded-sm'>
+                <h2 className='font-bold'>Llama 3</h2>
+                <h3>Base Size: 10GB</h3>
+                <h3>Number of Variations: 3</h3>
                 <Button onClick={handleRedirect}>Pull Locally</Button>
                 <Button>Pull on Servers</Button>
+                <Button>More Info</Button>
             </div>
         
         </>
