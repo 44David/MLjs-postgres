55252200e0a471939bda2a5b7d4236da49955ae9
David Soane <david.soane@outlook.com>
2024-07-11 18:57:01 -0600

    added UI components to pages

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -2,12 +2,46 @@ import ollama from 'ollama'
 import { Button } from "@/components/ui/button"
 import { db } from "@/server/db";
 import Link from 'next/link';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { HardDriveDownload, Database } from 'lucide-react';
 
 export default function Models() {
     return (
         <>
-            <h1 className='font-bold text-7xl mb-4'>Currently available models</h1>
-            <div className="grid gap-4 grid-cols-3 grid-rows-3">
+
+        <h1 className='font-bold text-7xl mb-4'>Currently available models</h1>
+
+        <NavigationMenu>
+        <NavigationMenuList>
+            <NavigationMenuItem>
+            <NavigationMenuTrigger>NIVIDA Nim Models</NavigationMenuTrigger>
+            <NavigationMenuContent>
+                <NavigationMenuLink className='p-6 w-full h-full'>Lllama3</NavigationMenuLink>
+            </NavigationMenuContent>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+        </NavigationMenu>
+            
+            <div className="grid gap-4 grid-cols-3 grid-rows-3 mt-10">
                     <ShowModels></ShowModels>
             </div>
             
@@ -28,8 +62,24 @@ export default function Models() {
                         <h3>Size: {modelInfo.size}</h3>
                         <h3>Context Length: {modelInfo.context_length}</h3>
                         <h3>Number of Variations: {modelInfo.variations}</h3>
-                        <Link href={`models/install/${modelInfo.id}`}><Button className='mr-2'>Pull Locally</Button></Link>
-                        <Button>Pull on Servers</Button>
+                        <Link href={`models/install/${modelInfo.id}`}><Button className='mr-2'> <HardDriveDownload className='h-4 w-4 mr-2'/> Pull Locally</Button></Link>
+
+                        <DropdownMenu>
+                            <DropdownMenuTrigger><Button><Database className='h-4 w-4 mr-2' />Pull on Servers</Button></DropdownMenuTrigger>  
+                            <DropdownMenuContent>
+
+                                <DropdownMenuLabel>Choose where to host</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Host on NVIDIA Nim</DropdownMenuItem>
+                                <DropdownMenuItem>Host from a bucket</DropdownMenuItem>
+                                <DropdownMenuItem>MLjs server (Network speeds will vary)</DropdownMenuItem>
+
+
+                            </DropdownMenuContent>
+                            
+
+                        </DropdownMenu>
+                        
                     </div>
                 </>
             ))
